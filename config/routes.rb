5a8f28f5519b4307f712ad88f09c709b3c7b289b
jocelyn <jocelynalvaradoruiz2021@gmail.com>
2025-10-30 22:49:23 -0600

    guia9 bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,16 @@
-Rails.application.routes.draw do 
+Rails.application.routes.draw do
+  # Página principal
   root 'static_pages#home'
-  
-  get "static_pages/help"
-  get "static_pages/about"
-   get "static_pages/contact"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # Rutas con nombre
+  get '/help',    to: 'static_pages#help',    as: 'help'
+  get '/about',   to: 'static_pages#about',   as: 'about'
+  get '/contact', to: 'static_pages#contact', as: 'contact'
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+  # Ruta de health check
+  get 'up' => 'rails/health#show', as: :rails_health_check
+
+  # Rutas comentadas para PWA (opcional)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
